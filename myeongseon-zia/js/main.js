f677abba0c0391ba81d251361e11421ae52dedde
Hong Gayoung <thehd9891@naver.com>
2023-09-20 14:19:39 +0900

    수정

--- myeongseon-zia/js/main.js
+++ myeongseon-zia/js/main.js
@@ -1,46 +1,53 @@
 
 // 계좌번호 복사하기
+const copy = (text) => {
+    // 임시의 textarea 생성
+    const $textarea = document.createElement("textarea");
+  
+    // body 요소에 존재해야 복사가 진행됨
+    document.body.appendChild($textarea);
+    
+    // 복사할 특정 텍스트를 임시의 textarea에 넣어주고 모두 셀렉션 상태
+    $textarea.value = text;
+    $textarea.select();
+    
+    // 복사 후 textarea 지우기
+    document.execCommand('copy');
+    document.body.removeChild($textarea);
+}
+
+
 var copybtn1 = document.querySelector('.copybtn1');
 var copyacc1= document.querySelector('.acc1').innerHTML;
-const copy1 = (text) => {
-    // 임시의 textarea 생성
-    const $textarea = document.createElement("textarea");
-  
-    // body 요소에 존재해야 복사가 진행됨
-    document.body.appendChild($textarea);
-    
-    // 복사할 특정 텍스트를 임시의 textarea에 넣어주고 모두 셀렉션 상태
-    $textarea.value = text;
-    $textarea.select();
-    
-    // 복사 후 textarea 지우기
-    document.execCommand('copy');
-    document.body.removeChild($textarea);
-}
 
 copybtn1.addEventListener('click', ()=>{
-    copy1(copyacc1);
+    copy(copyacc1);
+})
+copybtn1.addEventListener('touch', ()=>{
+    copy(copyacc1);
 })
 
-// 계좌번호 복사하기
+
+
+
 var copybtn2 = document.querySelector('.copybtn2');
 var copyacc2= document.querySelector('.acc2').innerHTML;
-const copy2 = (text) => {
-    // 임시의 textarea 생성
-    const $textarea = document.createElement("textarea");
-  
-    // body 요소에 존재해야 복사가 진행됨
-    document.body.appendChild($textarea);
-    
-    // 복사할 특정 텍스트를 임시의 textarea에 넣어주고 모두 셀렉션 상태
-    $textarea.value = text;
-    $textarea.select();
-    
-    // 복사 후 textarea 지우기
-    document.execCommand('copy');
-    document.body.removeChild($textarea);
-}
 
 copybtn2.addEventListener('click', ()=>{
-    copy2(copyacc2);
+    copy(copyacc2);
+})
+copybtn2.addEventListener('touch', ()=>{
+    copy(copyacc2);
+})
+
+
+
+var copybtn3 = document.querySelector('.copybtn3');
+var copyacc3= document.querySelector('.acc3').innerHTML;
+
+copybtn3.addEventListener('click', ()=>{
+    copy(copyacc3);
+})
+copybtn3.addEventListener('touch', ()=>{
+    copy(copyacc3);
 })
