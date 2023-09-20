626a03952327660e4e14931ef0eb02563a40c26f
Hong Gayoung <thehd9891@naver.com>
2023-09-20 15:51:36 +0900

    aaaa

--- myeongseon-zia/js/main.js
+++ myeongseon-zia/js/main.js
@@ -1,21 +1,4 @@
 
-// 계좌번호 복사하기
-// const copy = (text) => {
-//     // 임시의 textarea 생성
-//     const $textarea = document.createElement("input");
-//     $textarea.type = "text";
-//     // body 요소에 존재해야 복사가 진행됨
-//     document.body.appendChild($textarea);
-    
-//     // 복사할 특정 텍스트를 임시의 textarea에 넣어주고 모두 셀렉션 상태
-//     $textarea.value = text;
-//     $textarea.select();
-    
-//     // 복사 후 textarea 지우기
-//     document.execCommand('copy');
-//     document.body.removeChild($textarea);
-// }
-
 function copy (t){
     var textVal = t;
     window.navigator.clipboard.writeText(textVal).then(() => {
